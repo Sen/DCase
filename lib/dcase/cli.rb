1b0038a809934f4d2e095da0265549e6c409829b
Sen <sen9ob@gmail.com>
2014-01-30 14:23:48 +0800

    fix cli

--- lib/dcase/cli.rb
+++ lib/dcase/cli.rb
@@ -10,10 +10,10 @@ module DCase
         case @config.side
         when 'local'
           DCase::Local.new('0.0.0.0', 53, @crypto, @config)
-          puts "*** Local side is up, port:#{config.port}"
+          puts "*** Local side is up, port:#{@config.port}"
         when 'server'
           DCase::Server.new('0.0.0.0', @config.port, @crypto)
-          puts "*** Server side is up, port:#{config.port}"
+          puts "*** Server side is up, port:#{@config.port}"
         end
 
       puts "*** Hit Ctrl+c to stop"
