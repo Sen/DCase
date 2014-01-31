faa4bdbfeb662c303085a25a4fb062498f640ec5
Sen <sen9ob@gmail.com>
2014-01-31 11:21:48 +0800

    support config file now

--- lib/dcase/config.rb
+++ lib/dcase/config.rb
@@ -1,18 +1,50 @@
 require 'optparse'
+require 'yaml'
 
 require File.expand_path('../version', __FILE__)
 
 module DCase
   class Config
-    attr_accessor :args, :side, :server, :port, :password
+    attr_accessor :args, :side, :server, :port, :password, :config_path
 
     def initialize(args)
       @args = args || []
 
       parse_args
+      read_config
+
+      check_values
+    end
+
+    def check_values
+      if side.nil?
+        raise 'You need to define side of dcase in'
+      end
+
+      if server.nil?
+        raise 'You need to define remote server address'
+      end
+
+      if port.nil?
+        raise 'You need to define remote server port'
+      end
+
+      if password.nil?
+        raise 'You need to define a password, should be same in server/local side'
+      end
     end
 
     private
+
+    def read_config
+      @config_path = File.expand_path('../..', File.dirname(__FILE__)) + '/config.yml' unless @config_path
+      config = YAML.load_file @config_path
+
+      @side      = config["side"]          if @side.nil?
+      @server    = config["server"]        if @server.nil?
+      @port      = config["port"].to_i     if @port.nil?
+      @password  = config["password"]      if @password.nil?
+    end
 
     def parse_args
       opt_parser = OptionParser.new do |opts|
@@ -23,6 +55,10 @@ module DCase
 
         opts.on("-s", "--side SIDE", "Local or Server side") do |c|
           @side = c
+        end
+
+        opts.on("-c", "--config PATH", "Config file path") do |c|
+          @config_path = c
         end
 
         opts.on("-r", "--remote ADDRESS", "Remote server address") do |c|
